2ab6f3b335505974c17fe24d3ebf23e76dfec06c
lalocli <eduardo.ventura.lalocl@outlook.com>
2023-06-04 11:01:50 -0300

    Add menu texts

--- Suitium/hooks/DrawText.hpp
+++ Suitium/hooks/DrawText.hpp
@@ -8,14 +8,14 @@
 
 // https://github.com/noche-x/client/blob/main/src/game.hpp
 #if _WIN32
-using DrawTextFunc = std::int64_t (*)(const char *, float, float, float, int, float, float, float, float, ...);
+using DrawTextFunc = std::int64_t (*)(const char *, float, float, float, unsigned int, float, float, float, float, ...);
 #elif __linux__
 using DrawTextFunc = std::int64_t (*)(const char *, int, int, int, float, float, float, float, float, float, float, void *);
 #endif
 
 void CSFormat(const char *format, std::stringstream& newFormatStream, std::va_list vaList);
 #if _WIN32
-std::int64_t DrawTextHookFunc(const char *format, float arg2, float arg3, float arg4, int flags, float arg6, float arg7, float arg8, float arg9, ...);
+std::int64_t DrawTextHookFunc(const char *format, float x, float y, float size, unsigned int flags, float red, float green, float blue, float alpha, ...);
 #elif __linux__
 std::int64_t DrawTextHookFunc(const char *format, int, int, int, float, float, float, float, float, float, float, void *);
 #endif
@@ -30,7 +30,7 @@ std::int64_t DrawTextHookFunc(const char *format, int, int, int, float, float, f
 static subhook::Hook *drawTextHook;
 
 #if _WIN32
-std::int64_t DrawTextHookFunc(const char *format, float arg2, float arg3, float arg4, int flags, float arg6, float arg7, float arg8, float arg9, ...)
+std::int64_t DrawTextHookFunc(const char *format, float x, float y, float size, unsigned int flags, float red, float green, float blue, float alpha, ...)
 {
     subhook::ScopedHookRemove scopedRemove(drawTextHook);
 
@@ -38,17 +38,32 @@ std::int64_t DrawTextHookFunc(const char *format, float arg2, float arg3, float 
     DrawTextFunc originalFunc = (DrawTextFunc)(GetBaseAddress() + addressTable.at(AddressType::DrawTextFunc));
 
     std::stringstream newFormatStream;
-    if((flags & 0x40) == 0) // This flag is whether the text should be formatted or not
+    if((flags & 0x40) == 0) // This flag makes the text unformatted
     {
         std::va_list vaList;
-        va_start(vaList, arg9);
+        va_start(vaList, alpha);
         CSFormat(format, newFormatStream, vaList);
         va_end(vaList);
     }
     else
         newFormatStream << format;
 
-    return originalFunc(newFormatStream.str().c_str(), arg2, arg3, arg4, flags, arg6, arg7, arg8, arg9);
+    unsigned int newFlags = flags;
+    newFlags |= 0x40; // Add unformatted flag, since we already formatted it
+
+    if (std::strcmp(format, "Sub Rosa") == 0)
+    {
+        // The main menu is being drawn!
+        originalFunc("Suitium", x, y + 32, size * 0.85f, newFlags, 1.0f, 0.0f, 0.0f, 1.0f);
+    }
+    else if (std::strcmp(format, "W1nters") == 0)
+    {
+        // The last credits menu section is being drawn!
+        originalFunc("Suitium is made by", x, y + 64, size, newFlags, 1.0f, 0.0f, 0.0f, 1.0f);
+        originalFunc("JPXS", x + 120, y + 64, size, newFlags, 1.0f, 0.75f, 0.0f, 1.0f);
+    }
+
+    return originalFunc(newFormatStream.str().c_str(), x, y, size, newFlags, red, green, blue, alpha);
 }
 #elif __linux__
 std::int64_t DrawTextHookFunc(const char *format, int, int, int, float, float, float, float, float, float, float, void *)
@@ -74,7 +89,7 @@ void CSFormat(const char *format, std::stringstream& newFormatStream, std::va_li
         
         // I know I could've used a switch here
         if (format[formatCount] == '/')
-            newFormatStream << "//";
+            newFormatStream << '/';
         else if (format[formatCount] == 'c')
             newFormatStream << va_arg(vaList, char);
         else if (format[formatCount] == 'i')
