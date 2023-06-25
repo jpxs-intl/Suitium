833b7e87350233083cf62d5ff6f97c62989f551f
lalocli <eduardo.ventura.lalocl@outlook.com>
2023-06-25 20:30:54 -0300

    Fix JPXS credits for Linux (Oops!)

--- Suitium/hooks/CSDrawText.hpp
+++ Suitium/hooks/CSDrawText.hpp
@@ -165,8 +165,8 @@ std::int64_t CSDrawTextHookFunc(const char *format, unsigned int flags, int a, i
     else if (returnAddress == 0x8D488)
     {
         // The last credits menu section is being drawn!
-        addresses::CSDrawTextFunc("Suitium is made by", x, y + 64, size, flags, 1.0f, 0.0f, 0.0f, 1.0f);
-        addresses::CSDrawTextFunc("JPXS", x + 120, y + 64, size, flags, 1.0f, 0.75f, 0.0f, 1.0f);
+        addresses::CSDrawTextFunc("Suitium is made by", flags, a, b, x, y + 64, scale, 1.0f, 0.0f, 0.0f, 1.0f, c);
+        addresses::CSDrawTextFunc("JPXS", flags, a, b, x + 120, y + 64, scale, 1.0f, 0.75f, 0.0f, 1.0f, c);
     }
     else if (returnAddress == 0x15759D)
     {
