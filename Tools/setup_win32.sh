cc799827cf879d366fca9f401ead3150e23f1c40
lalocli <eduardo.ventura.lalocl@outlook.com>
2023-06-25 17:52:06 -0300

    Add setup_win32.sh

---
+++ Tools/setup_win32.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+if [ ! -d "Output"] 
+then
+    mkdir Output
+fi
+
+pushd Output
+mkdir moonjit
+cp -r ..\Modules\moonjit .\moonjit
+pushd moonjit
+make -j4
+popd
+popd
+
+echo /////////////
+echo If moonjit was compiled successfully, copy libluajit.so and liblua51.so to your game folder
+echo \\\\\\\\\\\\\\\\\\\\\\\\\\
