0304caafa3818f673fc046220a40a0c968a7d881
lalocli <eduardo.ventura.lalocl@outlook.com>
2023-06-25 22:18:54 -0300

    Fix setup_linux.sh

--- Tools/setup_linux.sh
+++ Tools/setup_linux.sh
@@ -5,10 +5,14 @@ then
     mkdir Output
 fi
 
-pushd Output
-
-popd
+cd Output
+mkdir moonjit
+cp -r ../Modules/moonjit ./
+cd moonjit
+make -j4
+cd ..
+cd ..
 
 echo /////////////
-echo If moonjit was compiled successfully, copy libluajit.so and liblua51.so to your game folder
-echo \\\\\\\\\\\\\\\\\\\\\\\\\\
+echo If moonjit was compiled successfully, copy libluajit.so to your game folder
+echo \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
